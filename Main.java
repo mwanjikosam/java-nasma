38674e2ca6617da66d30d444a98b16709924e599
mwanjikosam <nasmaoscar363@gmail.com>
2026-04-24 15:39:33 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public static void main(String[] args) {
+    String jina = "Juma";
+    int umri = 20;
+
+    System.out.println("Jina: " + jina);
+    System.out.println("Umri: " + umri);
+}
+
+
